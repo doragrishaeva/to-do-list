ddb2ac019b6bfc3b316c43c5b8fe0a33b46fd541
doragrishaeva <darya-grishaeva@mail.ru>
2020-08-13 18:11:52 +0300

    add jquery

--- script.js
+++ script.js
@@ -1,40 +1,49 @@
-//+когда input.value.length() > 0 и input не в focus, добавляем чек бокс
-//-когда чекбокс нажат, содержимое input зачеркивается
-//+когда нажимается + то клонируется input пустой 
-//+если содержимое строки стирается то чек бокс убирается
-//+когда нажимается чекбокс, то появляется галочка в нем
+//To be done:
+//adaptive design
+//keyup events
+//toggle checked symbol
+//style placeholders and plus button
+//text-decoration line-through on the whole input
 
-//add new empty string 
-const addNewButton = document.getElementById('add-new');
-addNewButton.onclick = function() {
-    let linesContainer = document.querySelector('.inputs-container');
-    let input = document.getElementById('input');
-    let clone = input.cloneNode(true);
-    clone.value="";
-    linesContainer.append(clone)
-};
+jQuery(document).ready(function() {
 
-//add empty checkbox
-const input = document.getElementById('line')
-input.onchange = function(event) {
-    let emptyBox = document.querySelector('.empty-box')
-    //add checkbox when new task had been added in a row
-    if (event.target.value.length >0) {
-        emptyBox.style.display = "inline-block";
-    //remove checkbox when row is empty
-    } else {
-        emptyBox.style.display = "none";
-    }
-}
+    //add new empty string 
+    $('#add-new').on('click', function() {
+        let linesContainer = $('.inputs-container');
+        //clone first input in container
+        let clone = $('.input:first').clone();
+        //clean new input value
+        $(clone).children().val('');
+        $(clone).children('span').hide();
+        //append clone to inputs container
+        linesContainer.append(clone);
+    });
 
-//add checked symbol
-const checkBox = document.getElementById('empty-box')
-checkBox.onclick = function() {
-    let checkedSymbol = document.getElementById('checked')
-    if (checkedSymbol.style.display === 'none') {
-        checkedSymbol.style.display = 'inline-block'
-    } else {
-        checkedSymbol.style.display = 'none'
-    }
-}
+    //add empty checkbox
+    $(document).on('change', '.line', function(e) {
+        let checkBox = $(e.target).parent().children('span');
+        //add checkbox if text had been added in a row
+        if (e.target.value.length >0) {
+            if(checkBox.length === 0) {
+                $(`<span class="empty-box"><svg width="20px" height="20px" aria-hidden="true" focusable="false" data-prefix="far" data-icon="square" class="svg-inline--fa fa-square fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#a9a9a9" d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm-6 400H54c-3.3 0-6-2.7-6-6V86c0-3.3 2.7-6 6-6h340c3.3 0 6 2.7 6 6v340c0 3.3-2.7 6-6 6z"></path></svg></span>`)
+                .appendTo($(e.target).parent()).show();
+            }
+            if(checkBox.length > 0 && checkBox.css({'display': 'none'})) {
+                checkBox.show();
+            }     
+        //remove checkbox when text has been erased
+        } else {
+            checkBox.css({'display': 'none'});
+            ($('.line').val().length === 0) ? $('.line').removeClass('line_checked') : $('.line').addClass('line_checked');
+        };
+    });
 
+    //toggle checked symbol and 'line-through' text property
+    $(document).on('click', '.empty-box', function(e) {
+        let currentInput = $(e.target).parent().parent().children()[0];
+        ($(currentInput).hasClass('line_checked')) ? $(currentInput).removeClass('line_checked') : $(currentInput).addClass('line_checked');
+        
+
+        //add function to toggle checked symbol
+    })
+})
