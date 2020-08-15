106b363e284af45336832ce106b7e31af797dcb1
doragrishaeva <darya-grishaeva@mail.ru>
2020-08-15 20:07:59 +0300

    main logic is ready

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 //To be done:
 //adaptive design
 //keyup events
-//toggle checked symbol
 //style placeholders and plus button
 //text-decoration line-through on the whole input
 
@@ -13,37 +12,32 @@ jQuery(document).ready(function() {
         //clone first input in container
         let clone = $('.input:first').clone();
         //clean new input value
-        $(clone).children().val('');
-        $(clone).children('span').hide();
+        $(clone).children('input').val('');
+        $(clone).children('svg').hide();
+        $(clone).children('input').removeClass('line_checked');
         //append clone to inputs container
         linesContainer.append(clone);
     });
 
-    //add empty checkbox
+    //show checkbox
     $(document).on('change', '.line', function(e) {
-        let checkBox = $(e.target).parent().children('span');
-        //add checkbox if text had been added in a row
+        //show transparent checkbox if text had been added in a row
         if (e.target.value.length >0) {
-            if(checkBox.length === 0) {
-                $(`<span class="empty-box"><svg width="20px" height="20px" aria-hidden="true" focusable="false" data-prefix="far" data-icon="square" class="svg-inline--fa fa-square fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#a9a9a9" d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm-6 400H54c-3.3 0-6-2.7-6-6V86c0-3.3 2.7-6 6-6h340c3.3 0 6 2.7 6 6v340c0 3.3-2.7 6-6 6z"></path></svg></span>`)
-                .appendTo($(e.target).parent()).show();
-            }
-            if(checkBox.length > 0 && checkBox.css({'display': 'none'})) {
-                checkBox.show();
-            }     
-        //remove checkbox when text has been erased
+            $(e.target).next().css('background-color', 'transparent');
+            $(e.target).next().show();
+
+        //hide checkbox when text has been erased
         } else {
-            checkBox.css({'display': 'none'});
-            ($('.line').val().length === 0) ? $('.line').removeClass('line_checked') : $('.line').addClass('line_checked');
+            $(e.target).next().hide();
+            $(e.target).removeClass('line_checked');
         };
     });
 
-    //toggle checked symbol and 'line-through' text property
-    $(document).on('click', '.empty-box', function(e) {
-        let currentInput = $(e.target).parent().parent().children()[0];
-        ($(currentInput).hasClass('line_checked')) ? $(currentInput).removeClass('line_checked') : $(currentInput).addClass('line_checked');
-        
 
-        //add function to toggle checked symbol
-    })
-})
+    //toggle 'line-through' text property
+    $(document).on('click', '.empty-box', function(e) {
+        let currentInput = $(e.target).parent().children('input');
+        ($(currentInput).hasClass('line_checked')) ? ($(currentInput).removeClass('line_checked'), $(e.target).css('background-color', 'transparent')) : ($(currentInput).addClass('line_checked'), $(e.target).css({'background-color':'green', 'background-size':'contain'}))
+    });
+
+});
